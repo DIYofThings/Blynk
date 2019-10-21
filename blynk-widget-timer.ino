42e7c5b872822f9132a089460a488f76abb76aef
DIY of Things <54387921+DIYofThings@users.noreply.github.com>
2019-10-21 15:31:39 +0700

    for youtube : https://youtu.be/N4ONjmnoL0g

---
+++ blynk-widget-timer.ino
@@ -0,0 +1,80 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP8266 chip.
+
+  Note: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right ESP8266 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "xxxxxxxxxxxxxxxxxxxxxxx";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "xxxxx";
+char pass[] = "xxxxx";
+
+BLYNK_WRITE(V5)
+{
+  // You'll get HIGH/1 at startTime and LOW/0 at stopTime.
+  // this method will be triggered every day
+  // until you remove widget or stop project or
+  // clean stop/start fields of widget
+  Serial.print("Got a value: ");
+  Serial.println(param.asInt());
+  
+  if(param.asInt() == 0)
+  {
+    digitalWrite(D4, HIGH);
+  }
+  if(param.asInt() == 1)
+  {
+    digitalWrite(D4, LOW);
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  pinMode(D4,OUTPUT);
+  digitalWrite(D4, HIGH);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
